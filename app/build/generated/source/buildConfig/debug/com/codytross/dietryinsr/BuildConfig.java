fa750857c2a7d957127b25037d4c490a08d55df5
Ivan Deschenaux <ivan.deschenaux@gmail.com>
2023-12-19 18:01:30 +0100

    Rejig main menu for new buttons

---
+++ app/build/generated/source/buildConfig/debug/com/codytross/dietryinsr/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.codytross.dietryinsr;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.codytross.dietryinsr";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
